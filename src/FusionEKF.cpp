806d6bac32b62617feadc89ba46eb77b90b267d5
Danilo Canivel <d.canivel@gmail.com>
2017-10-09 10:32:34 -0400

    Update FusionEKF.cpp

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -15,17 +15,14 @@ FusionEKF::FusionEKF() {
     is_initialized_ = false;
     previous_timestamp_ = 0;
 
-    // initializing matrices
     R_laser_ = MatrixXd(2, 2);
     R_radar_ = MatrixXd(3, 3);
     H_laser_ = MatrixXd(2, 4);
     Hj_ = MatrixXd(3, 4);
 
-    //measurement covariance matrix - laser
     R_laser_ << 0.0225, 0,
             0, 0.0225;
 
-    //measurement covariance matrix - radar
     R_radar_ << 0.09, 0, 0,
             0, 0.0009, 0,
             0, 0, 0.09;
@@ -46,10 +43,6 @@ FusionEKF::FusionEKF() {
             0, 0, 1, 0,
             0, 0, 0, 1;
     ekf_.Q_ = MatrixXd(4, 4);
-    // optimized noise parameters. Given the problem set, velocity prediction is
-    // fairly uncertain since it is only derived from radar measurements.
-    // Breaking that out into separate, high noise coefficients increased
-    // accuracy considerably.
     noise_px_ = 9;
     noise_py_ = 9;
     noise_vx_ = 100;
@@ -72,30 +65,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
           * Create the covariance matrix.
           * Remember: you'll need to convert radar from polar to cartesian coordinates.
         */
-        // first measurement
         cout << "EKF:" << endl;
         // initialize timestamp
         previous_timestamp_ = measurement_pack.timestamp_;
         if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-            /**
-            Convert radar from polar to cartesian coordinates and initialize state.
-            */
             const float px = measurement_pack.raw_measurements_(0) * cos(measurement_pack.raw_measurements_(1));
             const float py = measurement_pack.raw_measurements_(0) * sin(measurement_pack.raw_measurements_(1));
             const float vx = measurement_pack.raw_measurements_(2) * cos(measurement_pack.raw_measurements_(1));
             const float vy = measurement_pack.raw_measurements_(2) * sin(measurement_pack.raw_measurements_(1));
-            // initializing with vx and vy improves accuracy, but I don't like it.
-            // this works in both test cases because radar is aligned well with object
-            // at initial frame
+
             ekf_.x_ << px, py, vx, vy;
         } else {
-            /**
-            Initialize state.
-            */
             ekf_.x_ << measurement_pack.raw_measurements_(0), measurement_pack.raw_measurements_(1), 0, 0;
         }
-        // initialize covariance matrices
-        // initial state covariance
         ekf_.P_ << 1000, 0, 0, 0,
                 0, 1000, 0, 0,
                 0, 0, 1000, 0,
@@ -117,7 +99,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
        * Update the process noise covariance matrix.
      */
     float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0; //dt in seconds
-    // only update prior if a certain amount of time has passed
+
     if (dt > 0.0001) {
         previous_timestamp_ = measurement_pack.timestamp_;
         ekf_.F_(0, 2) = dt;
@@ -142,19 +124,15 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
        * Update the state and covariance matrices.
      */
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-        // Radar updates
         Hj_ = tools.CalculateJacobian(ekf_.x_);
         ekf_.H_ = Hj_;
         ekf_.R_ = R_radar_;
         ekf_.UpdateEKF(measurement_pack.raw_measurements_);
     } else {
-        // Laser updates
         ekf_.R_ = R_laser_;
         ekf_.H_ = H_laser_;
         ekf_.Update(measurement_pack.raw_measurements_);
     }
-
-    // print the output
     cout << "x_ = " << ekf_.x_ << endl;
     cout << "P_ = " << ekf_.P_ << endl;
 }
